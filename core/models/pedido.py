3fbaea5d11393e047321c6dc9161ac19357a7749
DanielaVenturi <danielaventuri14@gmail.com>
2024-08-29 09:01:21 -0300

    alteração das tabelas produto, edereco

--- core/models/pedido.py
+++ core/models/pedido.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.utils import timezone 
-from .categoria import Categoria
-
+from .produto import Produto
 
 class Pedido(models.Model):  
     data = models.DateField(auto_now_add=True)
-    categoria = models.ManyToManyField(Categoria, related_name="pedido", blank=True, null=True)
+    produto = models.ManyToManyField(Produto, related_name="pedidos", blank=True, null=True)
+
     def __str__(self):
-        return f"({self.id}) {self.data} "
-class Meta:
-    verbose_name = "Pedido"
-    verbose_name_plural = "Pedidos"
+        return f"({self.id}) {self.data} -- Total: R$ {self.get_total_value():.2f}"
+
+    def get_total_value(self):
+        return sum(produto.preco for produto in self.produto.all())
+
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
--- core/models/produto.py
+++ core/models/produto.py
@@ -1,12 +1,30 @@
 from django.db import models
+from .categoria import Categoria
 
+TAMANHOS_CHOICES = [
+    (33,'33'),
+    (34, '34'),
+    (35, '35'),
+    (36, '36'),
+    (37, '37'),
+    (38, '38'),
+    (39, '39'),
+    (40, '40'),
+    (41, '41'),
+    (42, '42'),
+    (43, '43'),
+    (44, '44'),
+]
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
+    tamanho = models.PositiveIntegerField(choices=TAMANHOS_CHOICES, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria = models.ManyToManyField(Categoria, related_name="produto", blank=True, null=True)
     def __str__(self):
         return f"({self.id}){self.nome} {self.preco} "
+    
 class Meta:
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
